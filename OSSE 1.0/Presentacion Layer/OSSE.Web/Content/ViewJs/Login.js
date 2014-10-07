9a2ccea70f9879fccf88b363259bf7e5ac989b0f
CamiCasus <mcarbajal@sigcomt.com>
2014-10-07 16:05:24 -0500

    Ajuste Login

--- OSSE 1.0/Presentacion Layer/OSSE.Web/Content/ViewJs/Login.js
+++ OSSE 1.0/Presentacion Layer/OSSE.Web/Content/ViewJs/Login.js
@@ -1,17 +1,9 @@
 ﻿jQuery(document).ready(function () {
-    //jQuery("#btnLogOn").on("click", function (evt) {
-    //    evt.preventDefault();
-        
-    //    var formulario = $("#frmLogOn");
-
-    //    if (formulario.validate().form()) {
-    //        formulario.submit();
-    //    } else {
-    //        alert("error");
-    //    }
-    //});
-    
-    jQuery("#frmLogOn").validate({
-        errorLabelContainer: jQuery("#frmLogOn div.valSumModel")
+    $('#frmLogin').on('submit', function () {
+        if (!$(this).valid()) {
+            return false;
+        }
+       
+        return true;
     });
 });
