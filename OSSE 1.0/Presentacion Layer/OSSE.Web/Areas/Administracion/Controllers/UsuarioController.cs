241be850cb3c09c064ee88c64f6cc2252435a265
CamiCasus <mcarbajal@sigcomt.com>
2014-10-14 13:34:06 -0500

    Inicio mantenimiento en usuarios.

---
+++ OSSE 1.0/Presentacion Layer/OSSE.Web/Areas/Administracion/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using OSSE.BusinessLogic.Interfaces;
+using OSSE.Web.Core;
+
+namespace OSSE.Web.Areas.Administracion.Controllers
+{
+    public class UsuarioController : BaseController
+    {
+        public UsuarioController(IFormularioBL formularioBL, IPermisoRolBL permisoRolBL, IItemTablaBL itemTablaBL)
+             :base(formularioBL, permisoRolBL, itemTablaBL)
+        {
+            
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
