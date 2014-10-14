e0652f61d6a7e60c2f3c41985826769c19800fd1
CamiCasus <mcarbajal@sigcomt.com>
2014-10-14 16:52:44 -0500

    Actualizacion de nombres de proyectos y de carpetas contenedoras.

---
+++ OSSE 1.0/Database Layer/SIGCOMT.Database.Generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambiar estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+
+[assembly: AssemblyTitle("SIGCOMT.DataBase.Generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SIGCOMT.DataBase.Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si ComVisible se establece en False, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+
+[assembly: Guid("b9677c74-f1cb-49bb-8548-b816a4d0fe9a")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el asterisco ('*'), como se muestra a continuación:
+// [ensamblado: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
