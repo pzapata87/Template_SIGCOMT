cd0c808e2e45df8423b3565cfd89ce7b252e7dc4
pzapata <pzapata@sigcomt.com>
2014-10-14 14:15:06 -0500

    Se creo archivo Utils para grilla

--- OSSE 1.0/Presentacion Layer/OSSE.Web/Content/ViewJs/Login.js
+++ OSSE 1.0/Presentacion Layer/OSSE.Web/Content/ViewJs/Login.js
@@ -1,10 +1,10 @@
-﻿jQuery(document).ready(function() {
+﻿jQuery(document).ready(function () {
     $('#frmLogin').on('submit', function(e) {
 
         if (!$(this).valid()) {
             e.preventDefault();
         }
-
+        
         return true;
     });
 });
---
+++ OSSE 1.0/Presentacion Layer/OSSE.Web/Content/js/utilsCommon.js
@@ -0,0 +1,42 @@
+﻿Utils = {
+    Grilla: function (opciones) {
+        if (opciones.dom == null)
+            opciones.dom = '<"left"f><"right"l>rtip';
+        
+        if (opciones.responsive == null)
+            opciones.responsive = true;
+        
+        if (opciones.processing == null)
+            opciones.processing = true;
+        
+        if (opciones.serverSide == null)
+            opciones.serverSide = true;
+        
+        if (opciones.order == null)
+            opciones.order = [[1, "desc"]];
+        
+        if (opciones.lengthMenu == null)
+            opciones.lengthMenu = [[5, 25, 50, -1], [5, 25, 50, "Todos"]];
+
+        var grilla = jQuery(opciones.grilla).dataTable({
+            "dom": opciones.dom,
+            "responsive": opciones.responsive,
+            "processing": opciones.processing,
+            "serverSide": opciones.serverSide,
+            "language": {
+                "url": opciones.languageUrl
+            },
+            "order": opciones.order,
+            "lengthMenu": opciones.lengthMenu,
+            "ajax": {
+                "url": opciones.url,
+                "type": "POST"
+            },
+            "columns": opciones.columns,
+            "fnInitComplete": function() {
+                // Permite reemplazar el evento de filtro por defecto del "keypress" por "enter"
+                grilla.fnFilterOnReturn();
+            }
+        });
+    }
+}
